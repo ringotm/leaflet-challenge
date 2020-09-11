391049b38ce19a03bd795a6a8620b32f11b3b37d
Tommy Ringo <ringotm@gmail.com>
2020-09-10 20:19:41 -0500

    finished map

--- Leaflet-Step-1/js/logic.js
+++ Leaflet-Step-1/js/logic.js
@@ -4,33 +4,6 @@ d3.json(link, function (data) {
     createFeatures(data.features);
 });
 
-//var geojsonMarkerOptions = {
-//    radius: 8,
-//   fillColor: "#ff7800",
-//   color: "#000",
-//   weight: 1,
-//   opacity: 1,
-//   fillOpacity: 0.8
-//};
-
-//function chooseColor(magnitude) {
-//   switch (magnitude) {
-//      case (magnitude > 4.5):
-//        return 'black';
-
-//  case (magnitude > 4):
-//    return 'blue';
-
-//case (magnitude > 3.5):
-//   return 'yellow';
-
-//case (magnitude > 3):
-//   return 'brown';
-
-//default:
-//   return 'green'
-// }
-//};
 
 function getColor(d) {
     return d >= 5 ? '#a50f15' :
@@ -55,7 +28,7 @@ function createFeatures(earthquakeData) {
                 color: "black",
                 weight: 1,
                 opacity: 1,
-                fillOpacity: 0.8
+                fillOpacity: 0.9
             };
 
 
@@ -72,43 +45,75 @@ function createFeatures(earthquakeData) {
 
 }
 
-//function createFeatures(earthquakeData) {
-//   var earthquakes = L.geoJSON(earthquakeData);
-// createMap(earthquakes);
-//}
+
+
+
+
 
 function createMap(earthquakes) {
-    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        maxZoom: 18,
-        id: "dark-v10",
-        accessToken: API_KEY
+    plates_url = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json";
+    d3.json(plates_url, function (plates_data) {
+        var myStyle = {
+            color: 'white',
+            fillOpacity: 0,
+            weight: 1
+        }
+        var plates = L.geoJSON(plates_data, { style: myStyle });
+
+        var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+            attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+            maxZoom: 18,
+            id: "dark-v10",
+            accessToken: API_KEY
+        });
+
+        var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+            attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+            maxZoom: 18,
+            id: "satellite-v9",
+            accessToken: API_KEY
+        });
+
+        var baseMaps = {
+            'Dark Map': darkmap,
+            'Satellite Map': satellitemap
+        };
+
+        var overlayMaps = {
+            "Earthquakes": earthquakes,
+            "Tectonic Plates": plates
+
+        };
+
+
+
+        var myMap = L.map("map-id", {
+            center: [17, 1.6596],
+            zoom: 3,
+            layers: [darkmap, earthquakes]
+        });
+
+        L.control.layers(baseMaps, overlayMaps, { collapsed: false }).addTo(myMap);
+
+        var legend = L.control({ position: "bottomright" });
+
+        legend.onAdd = function (map) {
+            var div = L.DomUtil.create("div", "legend");
+            div.innerHTML += "<h3>Magnitude</h3>";
+            div.innerHTML += '<i style="background: #fee5d9"></i><span>2.5-3.0</span><br>';
+            div.innerHTML += '<i style="background: #fcbba1"></i><span>3.0-3.5</span><br>';
+            div.innerHTML += '<i style="background: #fc9272"></i><span>3.5-4.0</span><br>';
+            div.innerHTML += '<i style="background: #fb6a4a"></i><span>4.0-4.5</span><br>';
+            div.innerHTML += '<i style="background: #de2d26"></i><span>4.5-5.0</span><br>';
+            div.innerHTML += '<i style="background: #a50f15"></i><span>5+</span><br>';
+
+
+
+
+            return div;
+
+        };
+        legend.addTo(myMap);
     });
-
-    var myMap = L.map("map-id", {
-        center: [28.0339, 1.6596],
-        zoom: 2,
-        layers: [darkmap, earthquakes]
-    });
-
-    var legend = L.control({ position: "bottomright" });
-
-    legend.onAdd = function (map) {
-        var div = L.DomUtil.create("div", "legend");
-        div.innerHTML += "<h3>Magnitude</h3>";
-        div.innerHTML += '<i style="background: #fee5d9"></i><span>2.5-3.0</span><br>';
-        div.innerHTML += '<i style="background: #fcbba1"></i><span>3.0-3.5</span><br>';
-        div.innerHTML += '<i style="background: #fc9272"></i><span>3.5-4.0</span><br>';
-        div.innerHTML += '<i style="background: #fb6a4a"></i><span>4.0-4.5</span><br>';
-        div.innerHTML += '<i style="background: #de2d26"></i><span>4.5-5.0</span><br>';
-        div.innerHTML += '<i style="background: #a50f15"></i><span>5+</span><br>';
-
-
-
-
-        return div;
-
-    };
-    legend.addTo(myMap);
 }
 
